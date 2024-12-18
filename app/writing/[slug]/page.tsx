0bf5f60760693b18e80b34ff6a9c3f7977f34904
SangharshSeth <sethsangharsh@outlook.com>
2024-12-19 01:14:46 +0530

    slug params fix

--- app/writing/[slug]/page.tsx
+++ app/writing/[slug]/page.tsx
@@ -2,6 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
+type Params = Promise<{ slug: string[] }>;
+
 export async function generateStaticParams() {
     const files = fs.readdirSync(path.join(process.cwd(), 'app/writing/posts'))
     return files.map(filename => ({
@@ -9,8 +11,8 @@ export async function generateStaticParams() {
     }))
 }
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
-    const { slug } = await params
+export default async function BlogPost({ params }: { params: Params }) {
+    const { slug } =  await params
     const filePath = path.join(process.cwd(), 'app/writing/posts', `${slug}.mdx`)
     const fileContent = fs.readFileSync(filePath, 'utf8')
 
